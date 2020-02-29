968cca4ed8c0e8df38db6f1b40267e1ee9b6ebe8
gnbutk777 <gnbutk777@gmail.com>
2020-02-29 23:53:04 +0200

    Updates

---
+++ precache-manifest.58e3e90f1b4a2465d21f83a153379c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b927835e539a38f020bcf4e119d35f73",
+    "url": "/emanuel-boukris-26-02-2020/index.html"
+  },
+  {
+    "revision": "337fd5fda5c2bc5b6e79",
+    "url": "/emanuel-boukris-26-02-2020/static/css/2.38c9a507.chunk.css"
+  },
+  {
+    "revision": "04e452f05dd626a09bd5",
+    "url": "/emanuel-boukris-26-02-2020/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "337fd5fda5c2bc5b6e79",
+    "url": "/emanuel-boukris-26-02-2020/static/js/2.05d2d477.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/emanuel-boukris-26-02-2020/static/js/2.05d2d477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e452f05dd626a09bd5",
+    "url": "/emanuel-boukris-26-02-2020/static/js/main.4f173fbc.chunk.js"
+  },
+  {
+    "revision": "e84e776a60007cb2c045",
+    "url": "/emanuel-boukris-26-02-2020/static/js/runtime-main.6e583ef6.js"
+  }
+]);
